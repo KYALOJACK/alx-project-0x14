994686c8f94a60de34e7e58eb69a4862d8c0a210
Jack Kilonzi <jackkyalo21@email.com>
2025-06-22 17:47:04 +0300

    Add files

--- alx-movie-app/components/layouts/Button.tsx
+++ alx-movie-app/components/layouts/Button.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import { ButtonProps } from "@/interfaces";
 
-const Button = () => {
-  return <button className="bg-green-500 text-white px-4 py-2 rounded">Submit</button>;
-};
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
+  return (
+    <button onClick={action} className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300">
+      {title}
+    </button>
+  )
+}
 
 export default Button;
